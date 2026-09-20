64389034630377b4a5011654da04c08aac62a9a1
timfesc <timfesc@gmail.com>
2026-09-20 13:24:46 +0500

    Add import for alembic in env.py

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+
 from alembic import context
 
 from bot.config.settings import get_settings
